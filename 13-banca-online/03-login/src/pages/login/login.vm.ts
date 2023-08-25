5ba7e3b885e33d2fb4920928bbe09b38aa79ff02
Braulio Diez Botella <braulio.diez@gmail.com>
2023-08-25 12:43:54 +0200

    validation no tests

--- 13-banca-online/03-login/src/pages/login/login.vm.ts
+++ 13-banca-online/03-login/src/pages/login/login.vm.ts
@@ -7,3 +7,13 @@ export const createEmptyCredentials = (): Credentials => ({
   user: "",
   password: "",
 });
+
+export interface CredentialsformErrors {
+  user: string;
+  password: string;
+}
+
+export const createEmptyCredentialsformErrors = (): CredentialsformErrors => ({
+  user: "",
+  password: "",
+});
