51e57822d1ce299aaf5752205b6c1aaf3cc30d8c
Braulio Diez Botella <braulio.diez@gmail.com>
2023-08-29 14:41:13 +0200

    wip

--- 13-banca-online/06-mis-cuentas/src/pages/account-list/account-list.page.tsx
+++ 13-banca-online/06-mis-cuentas/src/pages/account-list/account-list.page.tsx
@@ -1,29 +1,44 @@
 import React from "react";
-import { Link, generatePath } from "react-router-dom";
-import { appRoutes } from "@/core/router";
 import { AppLayout } from "@/layouts";
+import { AccountVm } from "./account-list.vm";
+
+const mockAccountListData: AccountVm[] = [
+  {
+    id: "1",
+    iban: "ES91 2100 0418 4502 0005 1332",
+    name: "Personal",
+    balance: "1490",
+    lastTransaction: new Date("2019-12-09T21:30:00"),
+  },
+  {
+    id: "2",
+    iban: "ES79 2100 0813 6101 2345 6789",
+    name: "Compartida",
+    balance: "2480",
+    lastTransaction: new Date("2019-11-21T14:07:38"),
+  },
+  {
+    id: "3",
+    iban: "ES66 2100 0418 4012 3456 7891",
+    name: "Ahorro",
+    balance: "8500",
+    lastTransaction: new Date("2019-11-15T08:29:04"),
+  },
+];
 
 export const AccountListPage: React.FC = () => {
+  const [accountList /*, setAccountList*/] =
+    React.useState<AccountVm[]>(mockAccountListData);
+
   return (
     <AppLayout>
       <div>
         Account List
-        <br />
-        <Link to={generatePath(appRoutes.movements, { id: 1 })}>
-          Movimientos de cuenta 1
-        </Link>
-        <br />
-        <Link to={appRoutes.transfer}>Transferencia</Link>
-        <br />
-        <Link to={generatePath(appRoutes.transferFromAccount, { id: 1 })}>
-          Transferencia desde cuenta 1
-        </Link>
-        <br />
-        <Link to={appRoutes.createAccount}>Crear cuenta</Link>
-        <br />
-        <Link to={generatePath(appRoutes.editAccount, { id: 1 })}>
-          Editar cuenta 1
-        </Link>
+        {accountList.map((account) => (
+          <div key={account.id}>
+            {account.name} - {account.balance}
+          </div>
+        ))}
       </div>
     </AppLayout>
   );
---
+++ 13-banca-online/06-mis-cuentas/src/pages/account-list/account-list.vm.ts
@@ -0,0 +1,7 @@
+export interface AccountVm {
+  id: string;
+  iban: string;
+  name: string;
+  balance: string;
+  lastTransaction: Date;
+}
