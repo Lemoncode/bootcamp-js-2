570630e4d73f2e91b65d7d4ab9edb959483e7d2c
Braulio Diez Botella <braulio.diez@gmail.com>
2023-09-03 12:43:47 +0200

    mis cuentas completed

--- 13-banca-online/06-mis-cuentas/src/pages/account-list/components/account-list-item.component.tsx
+++ 13-banca-online/06-mis-cuentas/src/pages/account-list/components/account-list-item.component.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { AccountVm } from "../account-list.vm";
 import classes from "./account-list-item.component.module.css";
-import { Link, generatePath } from "react-router-dom";
+import { Link, generatePath, useNavigate } from "react-router-dom";
 import { appRoutes } from "@/core/router";
+
+const ACTION_NONE = "";
+const ACTION_TRANSFER = "1";
+const ACTION_MOVEMENTS = "2";
 
 interface Props {
   accountItem: AccountVm;
@@ -10,6 +14,28 @@ interface Props {
 
 export const AccountListItemComponent: React.FC<Props> = (props) => {
   const { accountItem } = props;
+  const navigate = useNavigate();
+
+  const handleSelectedOptionChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
+    switch (e.target.value) {
+      case ACTION_TRANSFER:
+        navigate(
+          generatePath(appRoutes.transferFromAccount, {
+            id: accountItem.id,
+          })
+        );
+        break;
+      case ACTION_MOVEMENTS:
+        navigate(
+          generatePath(appRoutes.movements, {
+            id: accountItem.id,
+          })
+        );
+        break;
+    }
+  };
 
   return (
     <div className={classes.row}>
@@ -25,8 +51,15 @@ export const AccountListItemComponent: React.FC<Props> = (props) => {
       <span className={`${classes.dataCell} ${classes.alignRight}`}>
         {accountItem.lastTransaction.toLocaleDateString()}
       </span>
-      <span className={classes.dataCell}>
-        <select></select>
+      <span className={`${classes.dataCell} ${classes.selectContainer}`}>
+        <select
+          className={classes.select}
+          onChange={handleSelectedOptionChange}
+        >
+          <option value={ACTION_NONE}>Seleccionar</option>
+          <option value={ACTION_TRANSFER}>Transferir</option>
+          <option value={ACTION_MOVEMENTS}>Movimientos</option>
+        </select>
       </span>
     </div>
   );
